737e10c429eafe709d8a44eb779cf9cfde73e538
lydie <135144841+lydia22544@users.noreply.github.com>
2023-05-31 23:25:57 +0100

    Add files via upload

---
+++ book.js
@@ -0,0 +1,117 @@
+searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+} 
+let loginForm = document.querySelector('.login-form-container');
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+}
+document.querySelector('#close-login-btn').onclick = () =>{
+    loginForm.classList.remove('active');
+}
+
+
+window.onscroll = () =>{
+
+    searchForm.classList.remove('active');
+    if(window.scrollY > 80){
+        document.querySelector('.header .header-2').classList.add('active');
+    }else{
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+}
+
+window.onload = () =>{
+    if(window.scrollY > 80){
+        document.querySelector('.header .header-2').classList.add('active');
+    }else{
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+    fadeOut();
+}
+
+function loader(){
+    document.querySelector('.loader-container').classList.add('active');
+}
+function fadeOut(){
+    setTimeout(loader, 2000);
+}
+var swiper = new Swiper('.featured-slider', {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      spaceBetween:10,
+      loop:true,
+      consteredSlides:true,
+      slidesPerView:5,
+      autoplay:{
+          deplay:9500,
+          disableOnInteraction:false,
+      },
+      
+      breakpoints:{
+          0: {
+              slidesPerView:1,
+          },
+          768: {
+              slidesPerView:2,
+          },
+          1024: {
+              slidesPerView:3,
+          },
+          1024: {
+              slidesPerView:4,
+          },
+          1024: {
+              slidesPerView:5,
+          },
+      }
+          
+        });
+
+      var swiper = new Swiper(".arrivals-slider", {
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+        spaceBetween:10,
+        loop:true,
+        consteredSlides:true,
+        slidesPerView:5,
+        autoplay:{
+            deplay:9500,
+            disableOnInteraction:false,
+        },
+        
+        breakpoints:{
+            0: {
+                slidesPerView:1,
+            },
+            768: {
+                slidesPerView:2,
+            },
+            1024: {
+                slidesPerView:3,
+            },
+            1024: {
+                slidesPerView:4,
+            },
+            1024: {
+                slidesPerView:5,
+            },
+        }
+            
+          });
+
+          var swiper = new Swiper('.featured-slider', {
+              spaceBetween:10,
+              loop:true,
+              consteredSlides:true,
+              slidesPerView:5,
+              autoplay:{
+                  deplay:9500,
+                  disableOnInteraction:false,
+              },
+             });
